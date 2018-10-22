7ad563e72caf9155e373f86152ac007421018b20
Sachin Ahuja <sachinahj@gmail.com>
2018-10-22 16:14:58 -0700

    update montage create to create clips if not created

--- modules/montage__create/run.py
+++ modules/montage__create/run.py
@@ -1,5 +1,9 @@
+import ts_aws.dynamodb.clip
 import ts_aws.dynamodb.montage
+import ts_aws.dynamodb.stream
+import ts_aws.sqs.clip
 import ts_aws.sqs.montage
+import ts_aws.sqs.stream__initialize
 import ts_logger
 import ts_model.Montage
 import ts_model.Status
@@ -15,7 +19,55 @@ def run(event, context):
         logger.info("start", event=event, context=context)
         body = json.loads(event['body'])
         logger.info("body", body=body)
-        clip_ids = body['clip_ids']
+        clips = body['clips']
+
+        def get_clip_id(clip):
+            if 'clip_id' in clip:
+                return clip['clip_id']
+            else:
+                stream_id = clip['stream_id']
+                time_in = clip['time_in']
+                time_out = clip['time_out']
+
+                # get/initialize stream
+                try:
+                    stream = ts_aws.dynamodb.stream.get_stream(stream_id)
+                except ts_model.Exception as e:
+                    if e.code == ts_model.Exception.STREAM__NOT_EXIST:
+                        logger.error("warn", _module=f"{e.__class__.__module__}", _class=f"{e.__class__.__name__}", _message=str(e), traceback=''.join(traceback.format_exc()))
+                        stream = ts_model.Stream(
+                            stream_id=stream_id,
+                        )
+                        ts_aws.dynamodb.stream.save_stream(stream)
+
+                if stream._status_initialize == ts_model.Status.NONE:
+                    stream._status_initialize = ts_model.Status.INITIALIZING
+                    ts_aws.dynamodb.stream.save_stream(stream)
+                    ts_aws.sqs.stream__initialize.send_message({
+                        'stream_id': stream.stream_id,
+                    })
+
+                # create clip
+                clip_id = f"c-{shortuuid.uuid()}"
+                clip = ts_model.Clip(
+                    clip_id=clip_id,
+                    stream_id=stream_id,
+                    time_in=time_in,
+                    time_out=time_out,
+                    _status=ts_model.Status.INITIALIZING,
+                )
+
+                # save clip
+                ts_aws.dynamodb.clip.save_clip(clip)
+
+                # send job to clip
+                ts_aws.sqs.clip.send_message({
+                    'clip_id': clip.clip_id,
+                })
+
+                return clip_id
+
+        clip_ids = list(map(get_clip_id, clips))
 
         # create montage
         montage_id = f"m-{shortuuid.uuid()}"
--- modules/montage__create/test.py
+++ modules/montage__create/test.py
@@ -2,9 +2,13 @@ import run
 import json
 
 bodies = [{
-    'clip_ids': [
-        "c-qCuPKxsMpYPMPqjEzUJRsV",
-    ],
+    'clips': [{
+        'clip_id': 'c-zAXdUjbxFDjUXbDHtPfWR3',
+    } , {
+        'stream_id': 311038404,
+        'time_in': 1593.699742,
+        'time_out': 1598.455016,
+    }],
 }]
 
 for body in bodies:
